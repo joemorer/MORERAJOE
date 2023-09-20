ec35bf92c3f00e563161fb0740ac45865039ae6d
joemorer <78455317+joemorer@users.noreply.github.com>
2023-09-19 23:12:57 -0500

    Update prubaV2.py

--- pruba.py
+++ pruba.py
@@ -1,26 +1,53 @@
 import arcpy
 
 # Ruta al archivo shapefile de entrada
-input_shapefile = r"D:\Proyecto USTA-ECO\MGN2021_URB_MANZANA\MGN_URB_MANZANA.shp"
+input_shapefile = r"D:\Proyecto USTA-ECO\input\MGN2021_URB_MANZANA\MGN_URB_MANZANA.shp"
 
-# Ruta al directorio donde se creará el nuevo layer
-output_workspace = r"D:\Proyecto USTA-ECO\output"
+# Ruta al directorio donde se creará el nuevo shapefile
+output_workspace = r"D:\Proyecto USTA-ECO\working"
 
-# Nombre para el nuevo layer
-output_layer_name = "Top10Selection"
+# Nombre para el nuevo shapefile
+output_shapefile_name = "Top10Selection.shp"
 
-# Realizar la selección de las 10 primeras filas
+# Crear un nuevo shapefile con los primeros 10 registros
 arcpy.MakeFeatureLayer_management(input_shapefile, "SelectionLayer")
-arcpy.SelectLayerByAttribute_management("SelectionLayer", "NEW_SELECTION", "", "", "OID ASC")
-top_10_selection = arcpy.CopyFeatures_management("SelectionLayer", arcpy.Geometry())
+arcpy.SelectLayerByAttribute_management("SelectionLayer", where_clause="FID <= 10")
+output_shapefile_path = output_workspace + "\\" + output_shapefile_name
+arcpy.CopyFeatures_management("SelectionLayer", output_shapefile_path)
 
-# Crear un nuevo layer a partir de la selección
-output_layer_path = arcpy.CreateFeatureclass_management(output_workspace, output_layer_name,
-                                                        "POINT", spatial_reference=input_shapefile)
+print("Nuevo shapefile creado con la selección de los primeros 10 registros.")
 
-# Insertar las geometrías seleccionadas en el nuevo layer
-with arcpy.da.InsertCursor(output_layer_path, ["SHAPE@"]) as cursor:
-    for feature in top_10_selection:
-        cursor.insertRow([feature])
+# Obtener el mapa activo
+mxd = arcpy.mapping.MapDocument("CURRENT")
 
-print("Nuevo layer creado con las 10 primeras filas seleccionadas.")
+# Obtener el DataFrame activo
+df = arcpy.mapping.ListDataFrames(mxd)[0]
+
+# Nombre de la capa que deseas ocultar
+layer_to_hide_name = "SelectionLayer"
+
+# Buscar la capa por su nombre en el DataFrame
+for lyr in arcpy.mapping.ListLayers(mxd, "*", df):
+    if lyr.name == layer_to_hide_name:
+        lyr.visible = False  # Establecer la propiedad 'visible' en False para ocultar la capa
+
+print("Se ha ocultado el Layer: SelectionLayer")
+
+# Refrescar la vista
+arcpy.RefreshActiveView()
+
+# Hacer zoom a la capa "Top10Selection"
+for lyr in arcpy.mapping.ListLayers(mxd, "*", df):
+    if lyr.name == "Top10Selection":
+        arcpy.SelectLayerByAttribute_management(lyr, "CLEAR_SELECTION")  # Despejar cualquier selección
+        arcpy.SelectLayerByAttribute_management(lyr, "NEW_SELECTION", "FID <= 10")  # Seleccionar las primeras 10 filas
+        df.zoomToSelectedFeatures()  # Hacer zoom a las características seleccionadas
+
+# Refrescar la vista nuevamente
+arcpy.RefreshActiveView()
+
+# Activar la actualización de pantalla nuevamente
+arcpy.env.addOutputsToMap = True
+
+print("Cambios guardados en el mapa.")
+
